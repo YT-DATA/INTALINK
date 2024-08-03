33873efdaf1973fefd6f22b1c50fe955ea847b72
whx <1368596667@qq.com>
2024-08-03 19:18:35 +0800

    1. 根据需求变更进行重构

---
+++ src/main/java/com/intalink/configoperations/controller/eigenvalue/EigenvalueController.java
@@ -0,0 +1,48 @@
+package com.intalink.configoperations.controller.eigenvalue;
+
+import com.intalink.configoperations.domain.dataSource.IkBpDataSourceBasic;
+import com.intalink.configoperations.domain.dataSource.SDsnaDsSettingNew;
+import com.intalink.configoperations.mapper.dataSource.IkBpDataSourceBasicMapper;
+import com.intalink.configoperations.service.eigenvalue.EigenvalueService;
+import com.intalink.configoperations.utils.DataSourceUtil;
+import com.intalink.configoperations.web.controller.BaseController;
+import com.intalink.configoperations.web.domain.AjaxResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.Jedis;
+
+import java.io.InputStream;
+import java.util.Map;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+/**
+ * @author whx
+ * @date 2024/7/31
+ */
+@RestController
+@RequestMapping("/Eigenvalue")
+public class EigenvalueController extends BaseController {
+    @Autowired
+    private EigenvalueService eigenvalueService;
+
+    /**
+     * 将指定数据源数据存入Redis
+     */
+    @GetMapping(value = "/putData")
+    public AjaxResult putData(@RequestParam("dataSourceId") List<Integer> dataSourceId) {
+        try {
+            eigenvalueService.putData(dataSourceId);
+        } catch (OutOfMemoryError error) {
+            //自定义返回
+            return new AjaxResult(500, "内存溢出导致的错误", null);
+        }
+        return success();
+    }
+
+}
